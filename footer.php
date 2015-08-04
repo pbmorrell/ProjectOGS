67a8f1d59cc1fff49c727a6bf8b59d51559f2bde
Paul Morrell <pbmorrell@att.net>
2015-08-04 16:30:37 -0400

    Footer File Creation
    Created footer.php, added include to EventManager.php, and add CSS rule
    for newsletter markup to style.css

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!-- Footer Wrapper -->
+			<div id="footer-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+
+							<!-- Footer -->
+								 <footer id="footer">
+									<div class="row">
+										 <div class="6u">
+											
+											<section class="newsletter">
+												<h2>Become an Insider</h2>
+												<p>Sign up for our newsletter and never miss a beat.</p>
+												<input type="email" maxlength="50" placeholder=" email address">
+												<a href="#" class="button icon fa-sign-in">Sign Up!</a>
+											</section>
+										
+										</div> 
+										<div class="3u">
+										
+											<section>
+												<h2>Quick Links</h2>
+												<ul class="style3">
+													<li class="icon fa-newspaper-o">
+														<a href="" target="" style="text-decoration:none;"> &nbsp Recent News</a>
+													</li>
+													<li class="icon fa-cubes">
+														<a href="" target="" style="text-decoration:none;"> &nbsp About Us</a>
+													</li>
+												</ul>
+											</section>
+										
+										</div>
+										<div class="3u">
+										
+											<section>
+												<h2>Contact Us</h2>
+												<ul class="style3">
+													<li class="icon fa-envelope">
+														<a href="mailto:ProjectOGS@placeholder.com" target="" style="text-decoration:none;"> &nbsp Email</a>
+													</li>
+													<li class="icon fa-twitch">
+														<a href="http://www.twitch.tv/pbmorrell" target="_blank" style="text-decoration:none;"> &nbsp Twitch</a>
+													</li>
+												</ul>
+											</section>
+										
+										</div>
+									</div>
+								</footer>
+							<!-- Footer -->
+
+							<!-- Copyright -->
+								<div id="copyright">
+									&copy; <script>document.write(new Date().getFullYear());</script> Project OGS<br/> All Rights Reserved<br/>
+								</div>
+							<!-- Copyright -->
+						
+						</div>
+					</div>
+				</div>
+			</div>
+		<!-- Footer Wrapper -->
