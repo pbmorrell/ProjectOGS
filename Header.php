4c7017caf85e5ba248b21e3d0fdf959e12a8e8e4
Paul Morrell <pbmorrell@att.net>
2015-08-07 10:45:41 -0400

    Created base Header.php file
    Created the base Header.php file and updated the EventManager.php file
    to include the use of new file creation.

---
+++ Header.php
@@ -0,0 +1,44 @@
+<!-- Navigation Wrapper -->
+			<div id="header-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+						
+							<!-- Header -->
+								<header id="header">
+									
+									<!-- Logo -->
+										<h1><a href="#" id="logo">Project OGS</a></h1>
+									
+									<!-- Nav -->
+										<nav id="nav">
+											<ul>
+												<li>
+													<a href="" class="arrow">SteGiles01</a>
+													<ul>
+														<li><a href="#">Become a Premium Member</a></li>
+														<li><a href="#">Edit Profile</a></li>
+														<li><a href="#">Find Friends</a></li>
+														<li><a href="#">Log Out</a></li>
+													</ul>
+												</li>
+												<li>
+													<a href="" class="arrow">Menu</a>
+													<ul>
+														<li><a href="#">Recent News</a></li>
+														<li><a href="#">Developer Blog</a></li>
+														<li><a href="#">About Us</a></li>
+														<li><a href="#">Contact Us</a></li>
+													</ul>
+												</li>
+											</ul>
+										</nav>
+ 
+								</header>
+
+								</div>
+
+						</div>
+					</div>
+			</div>
+		<!-- Navigation Wrapper -->
