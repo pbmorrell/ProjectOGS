82ae5692172158cdfcb13dca07b21e75f2c2c97e
Paul Morrell <pbmorrell@att.net>
2015-12-21 12:21:03 -0500

    Updates to About.php and Faq.php
    Added actual readable text to Faq.php I'm still working on the css for
    this. It works perfect in my test environment but not so much on the
    live server.
    
    I changed About.php back to the original design. I remember you
    commenting on how you liked that design more. I tend to agree!

--- About.php
+++ About.php
@@ -16,69 +16,69 @@
     <body class="">
         <?php echo $headerHTML; ?>
         <!-- Main Wrapper -->
-	<div id="main-wrapper">
-            <div class="container">
-		<div class="row">
-                    <div class="12u">
-			<div id="main">
-                            <div class="row">
-				<div class="3u">
-                                    <!-- Features -->
-                                    <div id="sidebar">
-					<section class="box style2">
-                                            <h2>Stephen Giles</h2>
-                                            <div class="image">
-						<img src="images/author1.jpg" class="image full" alt="Stephen Giles"/>
-                                            </div>
-                                            <p>The master of all things programming, Stephen has graciously given much of his time to make this site a reality. When not hammering away on a keyboard, Stephen enjoys ping-pong, football, studying the Bible, and (occasionally) playing Halo and various racing games.</p>
-					</section>
-                                    </div>
+			<div id="main-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+							<div id="main">
+								<div class="row">
+									<div class="6u">
+										<!-- About -->
+										<div id="content">
+											<article class="box features">
+												<header>
+													<h2>About Us</h2>
+													<span class="byline"></span>
+												</header>
+												<img src="images/PlayerFind.jpg" class="image left" alt="Player Find"/>
+												<p>Have you ever started a new game only to wind up in a lobby with unfamiliar gamer tags? Are your current friends still playing a game you no longer have interest in? Do you need help with a particular game, but are unsure where to turn?</p>
+												<p>You've come to the right place!</p>
+												<p>Player Unite is dedicated to growing a community of gamers. Our site is your go-to place for meeting other gamers and setting up events to play with friends, old and new.</p>
+												<img src="images/HighFive.jpg" class="image right" alt="Snake & Samus Team Up" />
+												<p>Player Unite was created by Paul Morrell and Stephen Giles. We are a two man team dedicated to providing a simple way to unite video game players everywhere.</p>
+												<h2>Features</h2>
+													<article>
+														<ul><li class="icon fa-user">&nbsp Find new friends to play your favorite games with online.</li></ul>
+													</article>
+													<article>
+														<ul><li class="icon fa-rss-square">&nbsp Create an event that your friends or random users can sign up for. You decide who can join!</li></ul>
+													</article>
+													<article>
+														<ul><li class="icon fa-search">&nbsp A user generated event feed that allows you to search for games you are interested in.</li></ul>
+													</article>
+											</article>
+										</div>
+									</div>
+									<div class="3u">
+										<!-- Features -->
+										<div id="sidebar">
+											<section class="box style2">
+												<h2>Stephen Giles</h2>
+													<div class="image">
+														<img src="images/author1.jpg" class="image full" alt="Stephen Giles"/>
+                                                    </div>
+													<p>The mazster of all things programming, Stephen has graciously given much of his time to make this site a reality. When not hammering away on a keyboard, Stephen enjoys ping-pong, football, studying the Bible, and (occasionally) playing Halo and various racing games.</p>
+											</section>
+										</div>
+									</div>
+									<div class="3u">
+										<!-- Staff -->
+										<div id="sidebar2">
+											<section class="box style2">
+												<h2>Paul Morrell</h2>
+													<div class="image">
+														<img src="images/author2.jpg" class="image full" alt="Paul Morrell"/>
+													</div>
+													<p>The mold was broken when Paul entered this world. All joking aside, Paul put together the front end of the site. He enjoys all types of video games. From the FPS genre, to an involving RPG, and lets not forget the indies! Have you played Axiom Verge?!</p>
+											</section>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="6u">
-                                    <!-- About -->
-                                    <div id="content">
-					<article class="box features">
-                                            <header>
-						<h2>About Us</h2>
-						<span class="byline"></span>
-                                            </header>
-                                            <img src="images/PlayerFind.jpg" class="image left" alt="Player Find"/>
-                                            <p>Have you ever started a new game only to wind up in a lobby with unfamiliar gamer tags? Are your current friends still playing a game you no longer have interest in? Do you need help with a particular game, but are unsure where to turn?</p>
-                                            <p>You've come to the right place!</p>
-                                            <p>Player Unite is dedicated to growing a community of gamers. Our site is your go-to place for meeting other gamers and setting up events to play with friends, old and new.</p>
-                                            <img src="images/HighFive.jpg" class="image right" alt="Snake & Samus Team Up" />
-                                            <p>Player Unite was created by Paul Morrell and Stephen Giles. We are a two man team dedicated to providing a simple way to unite video game players everywhere.</p>
-                                            <h2>Features</h2>
-                                            <article>
-                                                <ul><li class="icon fa-user">&nbsp Find new friends to play your favorite games with online.</li></ul>
-                                            </article>
-                                            <article>
-                                                <ul><li class="icon fa-rss-square">&nbsp Create an event that your friends or random users can sign up for. You decide who can join!</li></ul>
-                                            </article>
-                                            <article>
-                                                <ul><li class="icon fa-search">&nbsp A user generated event feed that allows you to search for games you are interested in.</li></ul>
-                                            </article>
-                                        </article>
-                                    </div>
-                                </div>
-                                <div class="3u">
-                                    <!-- Staff -->
-                                    <div id="sidebar2">
-                                        <section class="box style2">
-                                            <h2>Paul Morrell</h2>
-                                            <div class="image">
-                                                <img src="images/author2.jpg" class="image full" alt="Paul Morrell"/>
-                                            </div>
-                                            <p>The mold was broken when Paul entered this world. All joking aside, Paul put together the front end of the site. He enjoys all types of video games. From the FPS genre, to an involving RPG, and lets not forget the indies! Have you played Axiom Verge?!</p>
-                                        </section>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+			</div>
         <!-- Footer Wrapper -->
         <?php include("Footer.php"); ?>
         <!-- Footer Wrapper -->
--- Faq.php
+++ Faq.php
@@ -16,76 +16,75 @@
     <body class="">
         <?php echo $headerHTML; ?>
         <!-- Main Wrapper -->
-	<div id="main-wrapper">
-            <div class="container">
-		<div class="row">
-                    <div class="12u">
-			<div id="main">
-                            <div class="row">
-				<div class="12u">
-                                    <!-- About -->
-                                    <div id="content">
-					<article class="box style1">
-                                            <header>
-						<h2>FAQ (Frequently Asked Questions )</h2><br/>
-                                                    <div id="accordion">
-							<h3>Section 1</h3>
-                                                             <div>
-								<p>
-								Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
-								ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
-								amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
-								odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
-								</p>
-                                                             </div>
-							<h3>Section 2</h3>
-                                                             <div>
-								<p>
-								Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-								purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-								velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-								suscipit faucibus urna.
-								</p>
-                                                            </div>
-							<h3>Section 3</h3>
-                                                            <div>
-								<p>
-								Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-								Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-								ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-								lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
-								</p>
-								<ul>
-								 <li>List item one</li>
-								 <li>List item two</li>
-								<li>List item three</li>
-								</ul>
-								 </div>
-							<h3>Section 4</h3>
-                                                            <div>
-								<p>
-								Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-								et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-								faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-								mauris vel est.
-								</p>
-								<p>
-								Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-								Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-								inceptos himenaeos.
-								</p>
-                                                             </div>
+			<div id="main-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+							<div id="main">
+								<div class="row">
+									<div class="12u">
+										<!-- About -->
+										<div id="content">
+											<article class="box style1">
+												<header>
+													<h2>FAQ (Frequently Asked Questions )</h2><br/>
+														<div id="accordion">
+															<h3>What is Player Unite?</h3>
+														  <div>
+															<p>
+															Player Unite is a "Looking for Group" message board. We have a simple to use interface that allows
+															you to find other players to game with.</p>
+															<p>
+															Sign up is quick and simple.&nbsp<a href="">Get Started Today!</a>
+															</p>
+														  </div>
+														  <h3>Is it free?</h3>
+														  <div>
+															<p>
+															Yes! The main components of Player Unite are completly free. This includes a personnel login,
+															unique profile, the ability to create and join gaming events, and access to search tools! The site has
+															been designed so anyone may use it and not be held back by member fees.</p>
+															<p>
+															Additional features can be obtained by subscribing.<br/>
+															Subscribing members (Premium Members) gain access to additional search tools, a freinds list,
+															and the ability to keep your gaming events private.
+															</p>
+														  </div>
+														  <h3>How do the Search Functions work?</h3>
+														  <div>
+															<p>
+															Straight Magic. No seriously, Stephen Giles knows magic.
+															</p>
+															<ul>
+															  <li>I'm still</li>
+															  <li>working</li>
+															  <li>on this!</li>
+															</ul>
+														  </div>
+														  <h3>Where do my donations go?</h3>
+														  <div>
+															<p>
+															All donations go towards the future of Player Unite. Your money gifts are 
+															reinvested into the server hosting cost, additioanl future features, improved integration,
+															marketing, and website maintenace.
+															</p>
+															<p>
+															Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+															Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+															inceptos himenaeos.
+															</p>
+														  </div>
+														</div>
+												</header>
+											</article>
+										</div>
+									</div>
+								</div>
 							</div>
-						</header>
-					</article>
-                                    </div>
-                                </div>
-                            </div>
+						</div>
+					</div>
+				</div>
 			</div>
-                    </div>
-		</div>
-            </div>
-	</div>
         <!-- Footer Wrapper -->
         <?php include("Footer.php"); ?>
         <!-- Footer Wrapper -->
