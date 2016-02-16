2cf3ad1590749d0d91ead5bc12f2205798dd11fc
Paul Morrell <pbmorrell@att.net>
2016-02-15 22:57:32 -0500

    About.php Update

--- About.php
+++ About.php
@@ -15,7 +15,7 @@
     </head>
     <body class="">
         <?php echo $headerHTML; ?>
-        <!-- Main Wrapper -->
+                <!-- Main Wrapper -->
 			<div id="main-wrapper">
 				<div class="container">
 					<div class="row">
@@ -30,46 +30,47 @@
 													<h2>About Us</h2>
 													<span class="byline"></span>
 												</header>
-												<img src="images/PlayerFind.jpg" class="image left" alt="Player Find"/>
-												<p>Have you ever started a new game only to wind up in a lobby with unfamiliar gamer tags? Are your current friends still playing a game you no longer have interest in? Do you need help with a particular game, but are unsure where to turn?</p>
+													<img src="images/PlayerFind.jpg" class="image rnd left" alt="Player Find"/>
+												<p>Have you ever started a new game only to wind up in a lobby with unfamiliar gamer tags? 
+													Are your current friends still playing a game you no longer have interest in? Do you need 
+													help with a particular game, but are unsure where to turn?</p>
 												<p>You've come to the right place!</p>
-												<p>Player Unite is dedicated to growing a community of gamers. Our site is your go-to place for meeting other gamers and setting up events to play with friends, old and new.</p>
-												<img src="images/HighFive.jpg" class="image right" alt="Snake & Samus Team Up" />
-												<p>Player Unite was created by Paul Morrell and Stephen Giles. We are a two man team dedicated to providing a simple way to unite video game players everywhere.</p>
-												<h2>Features</h2>
-													<article>
-														<ul><li class="icon fa-user">&nbsp Find new friends to play your favorite games with online.</li></ul>
-													</article>
-													<article>
-														<ul><li class="icon fa-rss-square">&nbsp Create an event that your friends or random users can sign up for. You decide who can join!</li></ul>
-													</article>
-													<article>
-														<ul><li class="icon fa-search">&nbsp A user generated event feed that allows you to search for games you are interested in.</li></ul>
-													</article>
-											</article>
+												<p>Player Unite is dedicated to growing a community of gamers. Our site is your go-to place 
+													for meeting other gamers and setting up events to play with friends, old and new.</p>
+													<img src="images/HighFive.jpg" class="image rnd right" alt="Snake & Samus Team Up" />
+												<p>Player Unite was created by Paul Morrell and Stephen Giles. We are a two man team dedicated 
+													to providing a simple way to unite video game players everywhere.</p>
+												<p>Sign up is quick and easy. Did we mention a standard account is free? <a href="Index.php" style="text-decoration:none;">
+													What are you waiting for?</a></p>
+												<p>Have questions? Be sure to check out our <a href="Faq.php" style="text-decoration:none;">FAQ</a> for additinal information.</p>
+										</article>
 										</div>
 									</div>
-									<div class="3u">
+									<div class="6u">
 										<!-- Features -->
-										<div id="sidebar">
-											<section class="box style2">
-												<h2>Stephen Giles</h2>
-													<div class="image">
-														<img src="images/author1.jpg" class="image full" alt="Stephen Giles"/>
-                                                    </div>
-													<p>The mazster of all things programming, Stephen has graciously given much of his time to make this site a reality. When not hammering away on a keyboard, Stephen enjoys ping-pong, football, studying the Bible, and (occasionally) playing Halo and various racing games.</p>
-											</section>
-										</div>
-									</div>
-									<div class="3u">
-										<!-- Staff -->
-										<div id="sidebar2">
-											<section class="box style2">
-												<h2>Paul Morrell</h2>
-													<div class="image">
-														<img src="images/author2.jpg" class="image full" alt="Paul Morrell"/>
-													</div>
-													<p>The mold was broken when Paul entered this world. All joking aside, Paul put together the front end of the site. He enjoys all types of video games. From the FPS genre, to an involving RPG, and lets not forget the indies! Have you played Axiom Verge?!</p>
+										<div id="content">
+											<section class="box style1">
+												<h2>Features</h2>
+													<p>
+														<ul><li class="icon fa-user">&nbsp Find new friends to play your favorite games with online.</li></ul>
+													</p>
+													<p>
+														<ul><li class="icon fa-rss-square">&nbsp Create an event that your friends or random users can sign up for. You decide who can join!</li></ul>
+													</p>
+													<p>
+														<ul><li class="icon fa-search">&nbsp A user generated event feed that allows you to search for games you are interested in.</li></ul>
+													</p>
+												<h2>The Team</h2>
+												<h3>Stephen Giles</h3>
+													<p>Stephen is well versed in multiple programming languages such as .NET, PHP, AJAX, and JS. 
+														When not stiching together backend databases, he enjoys a wide range of activties. Which include ping-pong, football, studying the Bible, 
+														playing the Halo series, and endless nights with Forza</p>
+												<h3>Paul Morrell</h3>
+													<p>During the day Paul is an Automotive Consultant for a number of body shops across the United States. 
+													In his free time he enjoys coding front end websites and gaming on the PS4 and Steam. He enjoys all types 
+													of video games. From the FPS genre, to an involving RPG, and lets not forget the indies! Have you played Axiom Verge?!
+													Paul is currently falling prey to the people of Yharnam in Bloodborne.
+													Follow Paul on Twitter <a href="https://twitter.com/pbmorrell" target="_blank" style="text-decoration:none;">@pbmorrell</a></p>
 											</section>
 										</div>
 									</div>
