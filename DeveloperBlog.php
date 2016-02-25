3047ada7420fd67fc3aa42e86f6d426a82c28a21
Paul Morrell <pbmorrell@att.net>
2016-02-25 15:45:41 -0500

    Developer Blog

--- DeveloperBlog.php
+++ DeveloperBlog.php
@@ -16,32 +16,37 @@
     <body class="">
         <?php echo $headerHTML; ?>
         <!-- Main Wrapper -->
-	<div id="main-wrapper">
-            <div class="container">
-		<div class="row">
-                    <div class="12u">
-			<div id="main">
-                            <div class="row">
-				<div class="12u">
-                                    <!-- About -->
-                                    <div id="content">
-					<article class="box style1">
-                                            <header>
-						<h2>Developer Blog</h2><br/>
-                                                    <p>Placeholder</p>
-                                            </header>
-                                                <article>
-                                                    <a href="#" class="button icon fa-question">Placeholder</a>
-                                                </article>
-					</article>
-                                    </div>
-                                </div>
-                            </div>
+			<div id="main-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+							<div id="main">
+								<div class="row">
+									<div class="12u">
+										<!-- About -->
+										<div id="content">
+											<article class="box style1">
+													<h2>Developer Blog</h2><br/>
+												<header>
+													<h3>Date: 02/26/2016</h3>
+													<p>Welcome to Player Unite! After months of writing code, we are thrilled to be pulling the curtain back.
+													The main focus of our site is to help unite gamers across the world. We accomplish this by hosting a 
+													living event feed where you can search, create, and sign up for gaming events across various platforms.
+													Stop in, create a profile, enjoy, and leave us some feedback. <br/> - The Devolopment Team</p>
+												</header>
+												<header>
+													<h3>Date: Cooming soon</h3>
+													<p>...More post to follow!</p>
+												</header>
+											</article>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-                    </div>
-		</div>
-            </div>
-	</div>
         <!-- Footer Wrapper -->
         <?php include("Footer.php"); ?>
         <!-- Footer Wrapper -->
